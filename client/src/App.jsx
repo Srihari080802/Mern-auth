0ab345991078ad9a2a57eec30bd04d783f51f57d
Srihari080802 <srihari8abi@gmail.com>
2023-09-01 11:59:23 +0530

    create and run th server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,21 +6,18 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 
-
 export default function App() {
   return (
     <BrowserRouter>
-    {/* header */}
-    <Header />
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  
-  </BrowserRouter>
+      {/* header */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
